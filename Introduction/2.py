0fb26cdb435dcfc3b1661541ddad739a85a3aa62
DHRUVIK LATHIYA <dhruviklathiya9999@gmail.com>
2024-07-07 18:40:31 +0530

    Escape sequence in detail

---
+++ Introduction/2.py
@@ -0,0 +1,28 @@
+# Auto calculation of python just like javascript
+print(4+4)
+print(4*3)
+
+# Everything else is same except for division
+    # By default python returns float value in single division operator
+print(4/2)
+print(2/4)
+    # We can make them integer by using double division operator || integer division
+print(4//2)
+print(2//4)
+
+# Power in python
+print(2**3) # Stands for 2^3 => 2 * 2 * 2
+
+# Find SQUARE ROOT
+print(2**0.5)
+
+# Get certain floating points
+print(round(2**0.5, 2))
+
+# Order of precedence in python
+# Parentheses
+# Power
+# Division # Multiplication # Modulo
+# addition # Substraction
+
+# Associative nature of parentheses is right to left which is opposite from left to right used in: # Division # Multiplication # Modulo # addition # Substraction
---
+++ Introduction/3.py
@@ -0,0 +1,16 @@
+# Variables in python
+name_of_variable = "Value of variable"
+print(name_of_variable)
+
+f_name = "D"
+l_name = "CODER"
+print(f_name+l_name)
+# Add space
+print(f_name+ " " + l_name)
+
+# Type convertion to string using str()
+_num = 100
+print(name_of_variable + " " + str(_num))
+
+# We cannot do arithmetic operations with string but we cal repeat string
+print(l_name * 10)
