33aeb53b05f02664d8d2a14a3558f61fdaed7fd3
DHRUVIK LATHIYA <dhruviklathiya9999@gmail.com>
2024-07-07 17:39:07 +0530

    Escape sequence

---
+++ Introduction/1.py
@@ -0,0 +1,27 @@
+# Printing number
+print(9)
+
+# Print strings
+print("Hello world")
+print('Hello world')
+
+# Print complex string
+print("Hello 'Single quotes in double quotes' ")
+print('Hello "Double quotes in single quotes" ')
+
+# Using escape sequence
+print("Hello \"Double quotes in double quotes\" ")
+print('Hello \'Single quotes in single quotes\' ')
+print("Line 1 \nLine 2")
+print("How to print backslash \\")
+print("We can print \ like this but at the end of line we must use \\")
+print("In python double backslash equals to single backslash")
+
+# Backspace escape sequence
+print("We have write helllo but output is hell\blo")
+
+# Print emoji in python using unicode:
+    # Write unicode from unicode.org & replace + with 000
+    # Unicode U+1F680 => U0001F680
+    # Put a backslash infront of code
+print("\U0001F680 \U0001F680 \U0001F680 \U0001F680 \U0001F680")
---
+++ Introduction/1exercise.py
@@ -0,0 +1,4 @@
+print("This is \\\\ double backslash")
+print("This is /\\/\\/\\ mountain")
+print("He is \tawesome")
+print("\\\" \\n \\t \\'")
