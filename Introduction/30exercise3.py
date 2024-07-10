47fc56dbcf8c42dfd8b5ddab2bb6e1690c7adcf2
DHRUVIK LATHIYA <dhruviklathiya9999@gmail.com>
2024-07-10 13:58:42 +0530

    DSA duplicate number from list

--- Introduction/30exercise3.py
+++ Introduction/30exercise3.py
@@ -14,5 +14,12 @@ if len(_list1) > len(_list2):
                 _duplicate.append(int(_list1[i]))
             else:
                 pass
+else:
+    for i in range(0,n):
+        for j in range(0,m):
+            if int(_list2[i]) == int(_list1[j]):
+                _duplicate.append(int(_list2[i]))
+            else:
+                pass
 
 print(_duplicate)
