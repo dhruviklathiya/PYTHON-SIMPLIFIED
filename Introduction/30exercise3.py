e2fdecbeba6144aa787aa71592dab115d4146184
DHRUVIK LATHIYA <dhruviklathiya9999@gmail.com>
2024-07-10 13:55:32 +0530

    DSA duplicate number from list

---
+++ Introduction/30exercise3.py
@@ -0,0 +1,18 @@
+# 2 list => Find out comman element and create new list containing common elements
+
+_list1 = input("Enter list element seperated by comma").split(",")
+_list2 = input("Enter list element seperated by comma").split(",")
+_duplicate = []
+
+n = max(len(_list1),len(_list2))
+m = min(len(_list1),len(_list2))
+
+if len(_list1) > len(_list2):
+    for i in range(0,n):
+        for j in range(0,m):
+            if int(_list1[i]) == int(_list2[j]):
+                _duplicate.append(int(_list1[i]))
+            else:
+                pass
+
+print(_duplicate)
