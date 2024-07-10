a127bf7bc97d5348a410948aeec58db8d556e410
DHRUVIK LATHIYA <dhruviklathiya9999@gmail.com>
2024-07-10 14:49:09 +0530

    Tuple unpacking

---
+++ Introduction/33.py
@@ -0,0 +1,11 @@
+# Tuple unpacking
+    # Same as destructuring in javascript but we must have to unpack all elements
+_tuple = (1,2,3,4,5)
+_one,_two,_three,_four,_five = _tuple
+print(_one)
+print(_two)
+print(_three)
+print(_four)
+print(_five)
+
+# We cannot change tuple but list inside tuple are muttable
