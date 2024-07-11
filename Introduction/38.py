1ff33bdfa1a6b7c94116e6c5dcdcc5ee5e3e5d45
DHRUVIK LATHIYA <dhruviklathiya9999@gmail.com>
2024-07-11 12:31:06 +0530

    Dictonary update

---
+++ Introduction/38.py
@@ -0,0 +1,15 @@
+# Use .update() for updating a dictonary
+
+_dictonary = {
+    "fname":"D",
+    "lname":"CODER"
+}
+
+_new_data_for_dictonary = {
+    "fname":"DHRUVIK",
+    "portfolio":"bento.me/dhruvik"
+}
+
+_dictonary.update(_new_data_for_dictonary)
+
+print(_dictonary)
