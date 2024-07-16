b4285f72474e66ac55e0a708b2c35d90b0d49247
DHRUVIK LATHIYA <dhruviklathiya9999@gmail.com>
2024-07-16 11:57:45 +0530

    * operator in zip

--- Introduction/57.py
+++ Introduction/57.py
@@ -3,7 +3,7 @@
 
 _tuple = ((1,11),(2,22),(3,33),(4,44),(5,55))
 
-_list1, _list2 = list(zip(*_tuple))
+_list1, _list2 = zip(*_tuple)
 
-print(_list1)
-print(_list2)
+print(list(_list1))
+print(list(_list2))
