951fed772e176e441f46e0c4f9034caafaf2fd57
dhruviklathiya <dhruviklathiya9999@gmail.com>
2024-07-22 15:49:00 +0530

    New methods for same task

--- Introduction/17.py
+++ Introduction/17.py
@@ -9,3 +9,11 @@ while i < len(_number):
 print(f"Sum of all digits in number is {result}")
 
 # Working : Do the same program using number as input
+_number = int(input("Enter number:"))
+_sum = 0
+while _number>0:
+    _sum+= _number%10
+        # Adding last digit in sum
+    _number = _number // 10
+        # Removing last digit for next iteration
+print(_sum)
