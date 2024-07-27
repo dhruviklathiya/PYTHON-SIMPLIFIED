8d0ef825062c0aef1726ea8d84cf6e483f1b9c72
DHRUVIK LATHIYA <dhruviklathiya9999@gmail.com>
2024-07-27 16:04:01 +0530

    Decorator function

---
+++ Introduction/69.py
@@ -0,0 +1,13 @@
+# Decorator function with arguments returning calculation
+def decorator_function(_function):
+    def inner_function(*args,**kwargs):
+        print("This is extended line")
+        return _function(*args,**kwargs)
+    return inner_function
+
+@decorator_function
+def sum_calculation(_para1,_para2):
+    print(f"This function takes argument _para as {_para1} & {_para2}")
+    return _para1 + _para2
+
+print(sum_calculation(20,3032))
