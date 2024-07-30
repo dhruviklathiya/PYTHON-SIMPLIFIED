6ddc65820e06dd8108dd6db3752799316af14a4a
DHRUVIK LATHIYA <dhruviklathiya9999@gmail.com>
2024-07-30 19:24:08 +0530

    OOP in our code

--- OOP/2.py
+++ OOP/2.py
@@ -1,7 +1,13 @@
 _list = [1,2,3,4,5]
     # Here we have defined a list but what we have actually done is created an instance(object) of list class
-        # list and many other class are provided by python by default
+        # list, string, tuple  and many other class are provided by python by default
         # For example
 _list.append(6)
         # Here we have used append method which is part of list class
 print(_list)
+
+# Definition of class:
+    # Class is a blueprint of an object
+
+# Definition of object:
+    # Objejct is an instance of class
