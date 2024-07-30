7d4103d158dfdaf3009245bdca195de3cb6faf4c
DHRUVIK LATHIYA <dhruviklathiya9999@gmail.com>
2024-07-30 23:57:52 +0530

    Python class

--- OOP/3.py
+++ OOP/3.py
@@ -4,8 +4,11 @@ class Car:
     def __init__(self, carname, carprice):
         self.carname = carname
         self.carprice = carprice
+        self.car_price = carprice
 
 car1 = Car("Venue","70000$")
 print(car1.carname)
 print(car1.carprice)
+# We can also change name of variable
+print(car1.car_price)
 
