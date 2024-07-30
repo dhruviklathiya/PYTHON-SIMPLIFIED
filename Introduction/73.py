acd573381c22928c3d017162b8d900fc1c40d5ec
DHRUVIK LATHIYA <dhruviklathiya9999@gmail.com>
2024-07-30 16:57:06 +0530

    Generator

--- Introduction/73.py
+++ Introduction/73.py
@@ -3,6 +3,7 @@
         # While traditional iterator takes up bulk amount of space in memory for iteration, generator only takes up place of single element
     # Use: Not asking python to run full loop instead we get output in sequence as we want
         # Generator will not provide output unless we ask for it
+import time
 
 def our_generator(_number):
     for i in range(1,_number+1):
@@ -12,3 +13,20 @@ print(our_generator(10)) # Generator
 
 for i in our_generator(10):
     print(i)
+
+# Let's comapre time taken by traditional loop and generator
+t1 = time.time()
+for i in range(0,11):
+    print(i)
+t2 = time.time()
+print(f"Final time is for simple loop {t2-t1}")
+
+# Generator time
+t1 = time.time()
+def our_generator(_number):
+    for i in range(0,_number+1):
+        yield i
+for i in our_generator(11):
+    print(i)
+t2 = time.time()
+print(f"Final time is for generator ==> {t2-t1}")
