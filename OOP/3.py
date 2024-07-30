ec44f0cbb2dd022d60bed4b1b80e6687f7d49c92
DHRUVIK LATHIYA <dhruviklathiya9999@gmail.com>
2024-07-30 19:46:55 +0530

    Python class

---
+++ OOP/3.py
@@ -0,0 +1,11 @@
+# Creating class in python
+
+class Car:
+    def __init__(self, carname, carprice):
+        self.carname = carname
+        self.carprice = carprice
+
+car1 = Car("Venue","70000$")
+print(car1.carname)
+print(car1.carprice)
+
