62aa0756fdfb661d1e563318dc730509f1089292
DHRUVIK LATHIYA <dhruviklathiya9999@gmail.com>
2024-07-30 19:21:54 +0530

    OOP in our code

---
+++ OOP/2.py
@@ -0,0 +1,7 @@
+_list = [1,2,3,4,5]
+    # Here we have defined a list but what we have actually done is created an instance(object) of list class
+        # list and many other class are provided by python by default
+        # For example
+_list.append(6)
+        # Here we have used append method which is part of list class
+print(_list)
