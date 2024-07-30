84481cc537b95f7d3ebbb400ed2b59c144a746c2
DHRUVIK LATHIYA <dhruviklathiya9999@gmail.com>
2024-07-30 17:57:46 +0530

    OOP

---
+++ OOP/1.py
@@ -0,0 +1,20 @@
+# OOP:
+    # OOP stands for object-oriented programming
+# OOPL:
+    # OOPL stands for object-oriented programming language
+
+# OOP is a style
+# OOPL is a language
+
+# Object-Oriented Programming (OOP) is a programming paradigm that uses objects and classes as its core components. It is designed to facilitate code reuse, modularity, and the organization of software by modeling real-world entities and their interactions.
+
+# Object-oriented programming language is a programming language that follows all 4 pillars of OOP:
+    # Inheritance
+    # Polymorphism
+    # Data abstraction || Abstraction
+    # Encapsulation
+# Shortkey: IPO DE
+
+# Properties of OOPL: Must have 4 pillars + classes and object
+
+# OOP is a powerful paradigm that promotes better organization and modularity of code, making it easier to develop, maintain, and scale software applications. Understanding and applying OOP principles is essential for modern software development.
