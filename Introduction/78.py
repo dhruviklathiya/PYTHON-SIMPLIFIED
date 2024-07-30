14fe269ea839773b8000f59692708922d3ac8777
DHRUVIK LATHIYA <dhruviklathiya9999@gmail.com>
2024-07-30 17:35:24 +0530

    Generator and time

---
+++ Introduction/78.py
@@ -0,0 +1,13 @@
+# Time taken for list Vs. generator
+# For finding how much memory is taken by both we must run this code with
+
+import time
+
+t1 = time.time()
+_list = [i for i in range(0,100000000)]
+# _list = [i for i in range(0,1000000000)] # Do not uncomment this if you have low speed processor
+print(f"Taken time by list: {time.time() - t1}")
+
+t1 = time.time()
+_generator_object = (i for i in range(0,1000000))
+print(f"Taken time by generator: {(time.time() - t1)}")
