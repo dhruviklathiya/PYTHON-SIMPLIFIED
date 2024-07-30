12b2ff356ac2031c789695dedc7854d330fd5fd9
DHRUVIK LATHIYA <dhruviklathiya9999@gmail.com>
2024-07-30 17:25:31 +0530

    Generator comprehension

---
+++ Introduction/77.py
@@ -0,0 +1,6 @@
+# Generator comprehension
+    # Syntax is same as list comprehension except parentheses instead of square bracket
+_gen_object = (i for i in range(0,11))
+
+for i in _gen_object:
+    print(i)
