c85b7b4df13fceefa58f17fa941c5fd7bf4950ac
DHRUVIK LATHIYA <dhruviklathiya9999@gmail.com>
2024-07-31 11:28:25 +0530

    Methods in class

--- OOP/4.py
+++ OOP/4.py
@@ -19,3 +19,16 @@ class Car:
 object1 = Car("Range Rover",2000,90000)
 _result = object1.sum_of_year_and_price()
 print(_result)
+
+class Person:
+    def __init__(self,first_name,last_name,middle_name,age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.middlename = middle_name
+        self.age = age
+    def full_name(self):
+        return (self.first_name +" "+ self.middlename +" "+ self.last_name)
+
+person1 = Person("John","Doe","Clay",20)
+_result = person1.full_name()
+print(_result)
