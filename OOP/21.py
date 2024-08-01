d5cb7ac7d5ed6bfe146f0a66633719044e48e1e7
DHRUVIK LATHIYA <dhruviklathiya9999@gmail.com>
2024-08-01 18:30:44 +0530

    Method resolution order & Method overriding

---
+++ OOP/21.py
@@ -0,0 +1,9 @@
+# Method resolution order:
+    # If method is not found in child class then python will look for that method in parent class
+
+# Method overriding:
+    # Both terminology are same
+    # If method is not found in child class then python will look for that method in parent class
+
+# Method resolution order is a list that python go through for checking available methods
+# Method overriding is a way how python runs method in inherited class
