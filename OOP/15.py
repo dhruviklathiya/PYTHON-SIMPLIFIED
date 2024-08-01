61f74a0d273e3d5a590cf2d000ddfed5109b2f3f
DHRUVIK LATHIYA <dhruviklathiya9999@gmail.com>
2024-08-01 11:34:41 +0530

    OOP concept

---
+++ OOP/15.py
@@ -0,0 +1,20 @@
+# Encapsulation:
+    # In simple words, grouping together of variables, and functions that are related
+    # Encapsulation is one of the fundamental concepts in object-oriented programming (OOP). It refers to the bundling of data (attributes) and methods (functions) that operate on the data into a single unit or class.
+
+# Abstraction:
+    # In simple words, using functions and methods without worrying about what is running underneath
+    # Abstraction is a core concept in object-oriented programming (OOP) that deals with hiding the complex implementation details and showing only the essential features of an object. It enables developers to work with high-level concepts without needing to understand the intricacies of how those concepts are implemented.
+
+# Example of abstraction:
+_list = [12,3,4,53,6]
+_list.sort()
+    # There are alot of sorting algorithms:
+                                        # Quick sort
+                                        # Merge sort
+                                        # Heap sort
+                                        # Bubble sort
+                                        # Insertion sort
+    # Pyhton is running sort method but we have no idea which sort is being run
+        # For knowledge: Python uses Timsort algorithm for sorting list
+print(_list)
