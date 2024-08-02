efdcefa09b56300f9db57bc4e7d9da45c0f457ee
DHRUVIK LATHIYA <dhruviklathiya9999@gmail.com>
2024-08-02 11:42:02 +0530

    Key error

---
+++ 03 ERROR/8.py
@@ -0,0 +1,11 @@
+# Key Error: Trying to access key inside dictionary that does not exist
+
+_dict = {
+    "Fname":"JOHN",
+    "Lname":"DOE"
+}
+
+print(_dict['Lname'])
+
+# Uncomment for error
+# print(_dict['lname'])
