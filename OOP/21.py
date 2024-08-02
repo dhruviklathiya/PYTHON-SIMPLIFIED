f4a39341c9bd31239a2f604c91cd4b53668506ba
DHRUVIK LATHIYA <dhruviklathiya9999@gmail.com>
2024-08-02 10:50:21 +0530

    Example of method resolution order

--- OOP/21.py
+++ OOP/21.py
@@ -1,6 +1,29 @@
 # Method resolution order:
     # If method is not found in child class then python will look for that method in parent class
 
+# We can use help() for finding order of resolution
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        pass
+
+# Base class 2
+class Bird:
+    def fly(self):
+        return f"{self.name} is flying."
+
+# Derived class
+class Parrot(Animal, Bird):
+    def speak(self):
+        return f"{self.name} says: Hello!"
+
+# Instantiate the Parrot class
+polly = Parrot("Polly")
+
+print(help(Parrot))
+
 # Method overriding:
     # Both terminology are same
     # If method is not found in child class then python will look for that method in parent class
