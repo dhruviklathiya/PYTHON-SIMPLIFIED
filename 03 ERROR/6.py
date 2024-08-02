edb6fb22a56812cab28196000932cee6fde644f5
DHRUVIK LATHIYA <dhruviklathiya9999@gmail.com>
2024-08-02 11:37:02 +0530

    Value error

---
+++ 03 ERROR/6.py
@@ -0,0 +1,10 @@
+# Value error: Occurs when proving wrong value
+
+# int() function converts string to integer but only if string contains some arithmetic data
+
+_string = "900"
+print(int(_string))
+
+# Uncomment for error
+# _string = "90A"
+# print(int(_string))
