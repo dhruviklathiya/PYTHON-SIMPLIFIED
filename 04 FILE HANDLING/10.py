6697eabfede1600ab88c1e861cb1196f4d866e1b
DHRUVIK LATHIYA <dhruviklathiya9999@gmail.com>
2024-08-06 18:40:25 +0530

    append mode

---
+++ 04 FILE HANDLING/10.py
@@ -0,0 +1,10 @@
+# Solution of not replacing content but adding is append mode in python
+
+
+with open('new_file.txt','a') as _file:
+    _file.write("\nThis is new file created by python\nThis is new line")
+
+_file = open('new_file.txt')
+print(_file.read())
+
+# We can notice that append mode did not replace all content inside our file but instead added
