86741ccff65b4d25588308891d17dcab21c8bd82
DHRUVIK LATHIYA <dhruviklathiya9999@gmail.com>
2024-08-06 16:52:26 +0530

    Python debugging

---
+++ 03 ERROR/18.py
@@ -0,0 +1,11 @@
+# Python debugging module
+import pdb
+
+pdb.set_trace()
+print("This is python debugger press l too see on which line you're and press n for debuggin next line:")
+_para1 = input("Enter your name:")
+_para2 = input("Enter your age:")
+
+print(_para1,_para2)
+
+# Suggestion: VS CODE itself has debugger refer: https://code.visualstudio.com/docs/python/debugging
