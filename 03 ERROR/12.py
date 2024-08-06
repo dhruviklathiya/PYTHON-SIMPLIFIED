8accb6403b53250405716177494088584a097035
DHRUVIK LATHIYA <dhruviklathiya9999@gmail.com>
2024-08-06 16:00:46 +0530

    ZeroDivisionError

---
+++ 03 ERROR/12.py
@@ -0,0 +1,8 @@
+# Exception handling
+    # Exception are errors and handling them so our code do not throw error on runtime
+# For example:
+def divide(a, b):
+    return a / b
+# Following function will throw error because it's performing unrealistic task
+result = divide(10, 0)
+# Output: ZeroDivisionError: division by zero
