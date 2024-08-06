9e0b29f5eb8c38dd446b95eedf170f5fbb1cefaa
DHRUVIK LATHIYA <dhruviklathiya9999@gmail.com>
2024-08-06 18:45:01 +0530

    r+ mode basics

---
+++ 04 FILE HANDLING/11.py
@@ -0,0 +1,11 @@
+# r+ mode in python
+
+# read mode => Read file
+# write mode => Write file (but with replacement)
+# append mode => Write file (adding data)
+# r+ mode => Read file & can overwrite (It does not replace whole data but overwrite the data)
+
+# read mode => No auto generation of file
+# write mode => Auto generation of file
+# append mode => Auto generation of file
+# r+ mode => No auto generation of file
